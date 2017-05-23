66234dd49dc737c33ad263ff654ac47ec9b18b9f
dylan-gregory <jdylangregory@gmail.com>
2017-05-23 10:07:05 -0400

    Working with react router

---
+++ modules/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router';
+import NavLink from './NavLink';
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>React Router Tutorial</h1>
+        <ul role="nav">
+          <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+          <li><NavLink to="/repos" activeStyle={{ color: 'red'}}>Repos</NavLink></li>
+        </ul>
+
+        {this.props.children}
+
+      </div>
+    )
+  }
+});
